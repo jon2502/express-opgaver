ac125f252518259c3cf9bafe8ea63730eb58ebdd
jon2502 <jonbjarke25@outlook.dk>
2023-01-23 11:08:26 +0100

    opgave opsat

---
+++ app.js
@@ -0,0 +1,44 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var frontpageRouter = require('./routes/frontpage');
+var userRouter = require('./routes/user');
+var op1Router = require('./routes/opgave_1');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', frontpageRouter);
+app.use('/opgave_1', op1Router);
+app.use('/user', userRouter);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
